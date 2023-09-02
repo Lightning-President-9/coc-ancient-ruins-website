4f67ceaa466fc4ab6e77098c2a4bf091a90de76f
Lightning-President-9 <122207673+Lightning-President-9@users.noreply.github.com>
2023-09-02 13:18:12 +0000

    added /api for mem and fmem

--- app.py
+++ app.py
@@ -4,11 +4,20 @@ from database import load_from_db_mem,load_from_db_fmem
 
 app = Flask(__name__)
 
+mem_list=load_from_db_mem()
+fmem_list=load_from_db_fmem()
+
 @app.route("/")
 def coc_ancient_ruins():
-    mem_list=load_from_db_mem()
-    fmem_list=load_from_db_fmem()
-    return render_template('home.html',DM=mem_list,DNM=fmem_list)
+  return render_template('home.html',DM=mem_list,DNM=fmem_list)
+
+@app.route("/api/mem")
+def data_mem():
+  return render_template('mem.html',DM=mem_list,DNM=fmem_list)
+
+@app.route("/api/fmem")
+def data_fmem():
+  return render_template('fmem.html',DM=mem_list,DNM=fmem_list)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
