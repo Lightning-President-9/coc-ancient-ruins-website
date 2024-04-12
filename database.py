57772c5f010a6b9a6f59295144b46a795a5451e0
Lightning-President-9 <122207673+Lightning-President-9@users.noreply.github.com>
2024-04-12 13:20:53 +0000

    Major Changes.
    Changed Database Service from PlanetScale to Neon. Used psycopg2-binary lib.

--- database.py
+++ database.py
@@ -3,22 +3,16 @@ import os
 
 db_connection_string=os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(db_connection_string,
-                       connect_args={
-                         "ssl":{
-                           "ssl_ca": "/etc/ssl/cert.pem"
-                         }
-                       }
-                      )
+engine = create_engine(db_connection_string)
 
 def load_from_db_mem():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from ClanMembers"))
-    mem_list= result.all()
-    return mem_list
+      with engine.connect() as conn:
+        result = conn.execute(text("select * from ClanMembers"))
+        mem_list= result.all()
+        return mem_list
 
 def load_from_db_fmem():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from FormerMembers"))
-    fmem_list= result.all()
-    return fmem_list
+      with engine.connect() as conn:
+        result = conn.execute(text("select * from FormerMembers"))
+        fmem_list= result.all()
+        return fmem_list
